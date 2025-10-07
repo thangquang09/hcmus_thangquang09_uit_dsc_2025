06751620c6b39da37dc683b34bf8fa13c303eb73
Ly Quang Thang <lythang09092004@gmail.com>
2025-10-07 10:57:58 +0700

    update

--- inference_allinone.sh
+++ inference_allinone.sh
@@ -1,7 +1,7 @@
-if [ ! -d "uit_dsc_lora_model_final_vllm" ]; then
+if [ ! -d "uit_dsc_lora_model_final_vllm_submit" ]; then
 	echo "Downloading VLLM..."
 	git lfs install
-    git clone https://huggingface.co/thangquang09/uit_dsc_lora_model_final_vllm
+    git clone https://huggingface.co/thangquang09/uit_dsc_lora_model_final_vllm_submit
 fi
 
 
@@ -10,7 +10,7 @@ pip install -r requirements.txt
 python3 main.py \
 	--mode inference \
 	--test_csv data/test/vihallu-private-test.csv \
-	--inference_model_path uit_dsc_lora_model_final_vllm \
+	--inference_model_path uit_dsc_lora_model_final_vllm_submit \
 	--model_name unsloth/Qwen3-4B-Instruct-2507 \
 	--max_seq_len 5000 \
 	--max_new_tokens 64 \
